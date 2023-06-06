a4e971edc0a87722305441d9147ac58b8ced42cf
GPSPic <picard.guillaume47@gmail.com>
2023-06-06 11:43:52 +0100

    create new components

---
+++ flappy-squirrel/components/Wall.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Matter from 'matter-js'
+import { View } from 'react-native'
+
+const Wall = (props : any) => {
+    const widthBody : number = props.body.bounds.max.x - props.body.bounds.min.x
+    const heightBody : number = props.body.bounds.max.y - props.body.bounds.min.y
+
+    const xBody = props.body.position.x - widthBody/2;
+    const yBody = props.body.position.y - heightBody/2;
+
+    const color = props.color
+
+    return (
+        <View style = {{
+            backgroundColor: color,
+            borderStyle: 'solid',
+            position: 'absolute',
+            left: xBody,
+            top: yBody,
+            width: widthBody,
+            height: heightBody
+        }}/>
+    )
+
+}
+
+export default (world: any, color: string, pos: any, size: any): any => {
+  
+    const initialWall: any = Matter.Bodies.rectangle(
+        pos.x,
+        pos.y,
+        size.width,
+        size.height,
+        {
+            label: 'Wall',
+            isStatic: true
+        }
+    )
+    Matter.World.add(world, initialWall)
+
+    return {
+        body : initialWall,
+        color,
+        pos,
+        renderer: <Wall/>
+    }
+}
--- flappy-squirrel/entities/index.tsx
+++ flappy-squirrel/entities/index.tsx
@@ -1,5 +1,9 @@
 import Matter from 'matter-js'
 import Squirrel from '../components/Squirrel';
+import Wall from '../components/Wall';
+import Floor from '../components/Floor';
+import Fish from '../components/Fish';
+import { Dimensions } from 'react-native';
 
 export default (restart: any) => {
     let engine = Matter.Engine.create({enableSleeping: false});
@@ -8,8 +12,16 @@ export default (restart: any) => {
 
     engine.gravity.y = 0;
 
+    const screenWidth: number = Dimensions.get("window").width;
+    const screenHeight: number = Dimensions.get("window").height;
+
     return{
         physics: {engine, world},
-        Squirrel: Squirrel(world, 'orange',{x:50, y:200}, {height: 40, width:40})
+        Squirrel: Squirrel(world, 'orange',{x:50, y:200}, {height: 40, width:40}),
+        Fish: Fish(world, 'blue',{x:200, y:200}, {width:10}),
+        WallLeft: Wall(world, 'brown',{x:0, y:screenHeight/2}, {height: screenHeight, width: 20}),
+        WallRight: Wall(world, 'brown',{x:screenWidth, y:screenHeight/2}, {height: screenHeight, width: 20}),
+        Floor: Floor(world, 'yellow',{x:screenWidth/2, y:screenHeight}, {height: 30, width: screenWidth-20}),
+        
     }
 }
