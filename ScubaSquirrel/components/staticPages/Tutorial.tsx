55cf4044723e08fcecd71ce97e17a050632c6262
Rodger Boyce <rodgerdboyce@gmail.com>
2023-06-08 12:20:36 +0100

    static pages create

---
+++ ScubaSquirrel/components/staticPages/Tutorial.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function About() {
+  return (
+    <View style = {{flex: 1, backgroundColor: "yellow"}}>
+      <Text>Tutorial</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
