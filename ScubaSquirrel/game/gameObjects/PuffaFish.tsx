492cd3f80cbcd0ef222b52babdce5543b232e83b
McMuds <clairemcmurdo@gmail.com>
2023-06-09 22:04:36 +0100

    Puffafish and organisation

--- ScubaSquirrel/game/gameObjects/PuffaFish.tsx
+++ ScubaSquirrel/game/gameObjects/PuffaFish.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Matter from 'matter-js'
-import { View } from 'react-native'
+import { View, Image } from 'react-native'
 
 const PuffaFish = (props : any) => {
     const widthBody : number = props.body.bounds.max.x - props.body.bounds.min.x
@@ -8,19 +8,34 @@ const PuffaFish = (props : any) => {
     const xBody = props.body.position.x - widthBody/2;
     const yBody = props.body.position.y - heightBody/2;
     const color = props.color
+    const imageURL = require('../../assets/puffafish.png');
 
     return (
-        <View style = {{
-            borderWidth:1,
-            borderColor:color,
-            borderStyle: 'solid',
-            position: 'absolute',
-            left: xBody,
-            top: yBody,
-            borderRadius: widthBody/2,
-            width: widthBody,
-            height: heightBody
-        }}/>
+        // <View style = {{
+        //     borderWidth:1,
+        //     borderColor:color,
+        //     borderStyle: 'solid',
+        //     position: 'absolute',
+        //     left: xBody,
+        //     top: yBody,
+        //     borderRadius: widthBody/2,
+        //     width: widthBody,
+        //     height: heightBody
+        // }}/>
+
+        <View>
+        <Image
+   source={imageURL} // Replace with the correct path to the GIF image
+   style={{
+     position: 'absolute',
+     left: xBody,
+     top: yBody,
+         borderRadius: widthBody/2,
+     width: widthBody,
+     height: heightBody,
+   }}
+       />
+   </View>
     )
 }
 
---
+++ ScubaSquirrel/game/utils/moveSquirrel.tsx
@@ -0,0 +1,22 @@
+import Matter from "matter-js";
+
+const moveSquirrel = (entities: any, screenTouch: any)=>{
+    screenTouch.filter((t: any) => (t.type === 'press'))
+        .forEach((t: any) => {
+        const touchX = (t.event.pageX);
+        const touchY = (t.event.pageY - 140);
+        const squirrelX = entities.Squirrel.body.position.x
+        const squirrelY = entities.Squirrel.body.position.y
+
+        const xVelo = ((touchX - squirrelX) / (Math.abs(touchX - squirrelX) + Math.abs(touchY - squirrelY)))*1
+        const yVelo = ((touchY - squirrelY) / (Math.abs(touchX - squirrelX) + Math.abs(touchY - squirrelY)))*1
+    
+    
+        Matter.Body.setVelocity(entities.Squirrel.body,{
+            x:xVelo,
+            y:yVelo
+        })                
+    })   
+} 
+
+export default moveSquirrel;
