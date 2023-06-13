3d886005e988b844c32379e373486746ff074e8e
McMuds <clairemcmurdo@gmail.com>
2023-06-13 13:24:16 +0100

    moving crab

---
+++ ScubaSquirrel/game/utils/moveCrab.tsx
@@ -0,0 +1,29 @@
+import Matter from "matter-js";
+import { Dimensions } from "react-native";
+
+let moveValue: number = 1;
+
+const moveCrab = (entities: any)=>{
+    const screenLeftPos: number = 50;
+    const screenRightPos: number = Dimensions.get('screen').width -50; 
+    const crabPosition: number = entities.Crab.body.position.x;
+    // console.log(`moveCrab: SLP, SRP, mV, CP: ${screenLeftPos}, ${screenRightPos}, ${moveValue}, ${crabPosition}`)
+
+    
+    Matter.Body.translate(entities.Crab.body,{
+        x:moveValue,
+        y:0
+    })
+
+    if(crabPosition >= screenRightPos ){
+        // Matter.Body.translate(entities.Crab.body,{x:-10,y:0})
+        moveValue = -1;
+        // console.log(`at right: ${crabPosition}`)
+    }
+    if(crabPosition <= screenLeftPos){
+        moveValue = 1;
+        // console.log(`at left: ${crabPosition}`)
+    }
+}    
+
+export default moveCrab;
