568137f6086961bebdb1b4f629a41c23c7f284e7
McMuds <clairemcmurdo@gmail.com>
2023-06-18 22:07:57 +0100

    slowed everyone down, and fixed Jelly movement

--- ScubaSquirrel/game/utils/moveJellyFish.tsx
+++ ScubaSquirrel/game/utils/moveJellyFish.tsx
@@ -8,26 +8,31 @@ const moveJellyFish = (entities: any) => {
   );
 
   let jellyFishEntities: any[] = [];
-  const screenLeftPos: number = 50;
-  const screenRightPos: number = Dimensions.get("window").width - 50;
+  // const screenLeftPos: number = 50;
+  // const screenRightPos: number = Dimensions.get("window").width - 50;
+  const screenHeight: number = Dimensions.get('window').height;
+  const screenWidth: number = Dimensions.get('window').width;
+  const screenTopPos: number = screenWidth/12*1.45 + 30;
+  const screenBottomPos: number =  screenHeight - screenHeight/7 - 30;
 
-  Object.values(filteredEntities).forEach((jellyFish: any) => {
-    if (jellyFish.body.label === 'JellyFish') {
-      const jellyFishPosition: number = jellyFish.body.position.x;
+  Object.values(filteredEntities)
+    .forEach((jellyFish: any) => {
+      if (jellyFish.body.label === 'JellyFish') {
+        const jellyFishPosition: number = jellyFish.body.position.y;
         
-
-      let moveValue;
+        let moveValue;
         
-      if (jellyFishPosition >= screenRightPos) {
-        jellyFish.direction *= -1
-      } else if (jellyFishPosition <= screenLeftPos) {
-        jellyFish.direction *= -1
+        if (jellyFishPosition <= screenTopPos) {
+          jellyFish.direction = 0.5
+        } else if (jellyFishPosition >= screenBottomPos) {
+          jellyFish.direction = -0.5
+        }
+        moveValue = jellyFish.direction 
+        // * generic.getRandomValue(1,3)
+        jellyFishEntities.push({ jellyFish, moveValue });
+      }
     }
-    moveValue = jellyFish.direction * generic.getRandomValue(1,3)
-      jellyFishEntities.push({ jellyFish, moveValue });
-  }
-});
-
+  );
 
   for (const item of jellyFishEntities) {
     Matter.Body.translate(item.jellyFish.body, {
@@ -36,7 +41,7 @@ const moveJellyFish = (entities: any) => {
     });
   }
 
-//   console.log(`fish mv: ${jellyFishEntities[0].moveValue}`);
+  // console.log(`Jelly mv: ${jellyFishEntities[0].moveValue}`);
 };
 
 export default moveJellyFish;
